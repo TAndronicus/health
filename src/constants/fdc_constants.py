db551dd0703308a2410a333105875f9e51848692
Jedrzej Biedrzycki <jed.biedrzycki@gmail.com>
2022-06-13 17:49:15 +0200

    Add more constants

--- src/constants/fdc_constants.py
+++ src/constants/fdc_constants.py
@@ -1,4 +1,4 @@
-class FoodConstants:
+class Foods:
     # Bread
     SUNFLOWER_BREAD = 1100825
     # Diary
@@ -7,14 +7,27 @@ class FoodConstants:
     CHIVES = 1103349
     # Fats
     FLAX_OIL = 2058238
+    # Sweets
+    LOTUS = 1984148
+    LADY_FINGERS = 2008520
 
 
 class Meals:
     BREAD_WITH_CHEESE_AND_FLAX_OIL = {
         'Bread with cheese and flax oil': {
-            FoodConstants.SUNFLOWER_BREAD: 100,
-            FoodConstants.DRY_COTTAGE_CHEESE: 50,
-            FoodConstants.CHIVES: 20,
-            FoodConstants.FLAX_OIL: 10
+            Foods.SUNFLOWER_BREAD: 100,
+            Foods.DRY_COTTAGE_CHEESE: 50,
+            Foods.CHIVES: 20,
+            Foods.FLAX_OIL: 10
+        }
+    }
+    LOTUS = {
+        'Lotus': {
+            Foods.LOTUS: 8
+        }
+    }
+    LADY_FINGERS = {
+        'Lady finger': {
+            Foods.LADY_FINGERS: 6
         }
     }
